1397dfe8039d527a1616fa356ec7b6555933e46f
anshulLuhsna <anshulkalbande@gmail.com>
2023-11-18 20:54:31 +0530

    Llm added

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -3,9 +3,12 @@ import React, { useState } from "react";
 function Prompt() {
   const [message, setMessage] = useState(""); // State to hold the message
   const [displayMessage, setDisplayMessage] = useState(false); // State to manage displaying the message
+  const [output, setOutput] = useState("")
 
   const handleMessageChange = (e) => {
-    setMessage(e.target.value); // Update the message as the user types
+    // setMessage(e.target.value); // Update the message as the user types
+    setMessage(e.target.value)
+    // setOutput("works")
     if (!e.target.value) {
       // If input is empty, hide the displayed message
       setDisplayMessage(false);
@@ -13,9 +16,73 @@ function Prompt() {
   };
 
   const handleSendClick = () => {
-    // Logic to handle sending the message
-    setDisplayMessage(true); // Show the message below after clicking Send (you can customize this logic)
-  };
+    setDisplayMessage(true)
+
+
+    let answer = ""
+  
+  var data = {
+    question:message
+}
+
+var json = JSON.stringify(data);
+
+fetch("https://cbce-34-125-187-64.ngrok-free.app/generate", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    
+  },
+  // mode:"no-cors",
+  body: json
+})
+.then(response => {
+    // Check if the response is OK
+    if (!response.ok) {
+      throw new Error('Network error');
+    }
+    // Get the response body as a ReadableStream
+    const stream = response.body;
+    // Get a reader for the stream
+    const reader = stream.getReader();
+    // Define a function to read the chunks recursively
+    function readChunk() {
+      // Read a chunk from the stream
+      reader.read().then(({ value, done }) => {
+        // Check if the stream is done
+        if (done) {
+          // Close the reader
+          reader.releaseLock();
+          // Return from the function
+          return;
+        }
+        // Process the chunk as JSON
+        const token = value;
+        const decoder = new TextDecoder("utf-8");
+        const text = decoder.decode(token);
+        // Do something with the token
+        console.log(text)
+        answer += text
+        setOutput(answer)
+        // Read the next chunk
+        readChunk();
+      });
+    }
+    // Start reading the first chunk
+    readChunk();
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+  });
+
+
+
+
+
+  
+    };
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -37,7 +104,7 @@ function Prompt() {
           <button onClick={handleSendClick}>Send</button>
         </div>
         {/* Display the message below the prompt after clicking Send */}
-        {displayMessage && <p>{message}</p>}
+        {displayMessage && <p>{output}</p>}
       </div>
     </div>
   );
